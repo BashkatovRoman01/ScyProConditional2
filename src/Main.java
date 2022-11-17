2fcaefacd5e3394998ed710ac8391383a76b042d
User <bashkatovr@mail.ru>
2022-11-17 16:39:28 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Задание 1
+        System.out.println("Задание 1");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println(" Установите версию приложения для Android по ссылке. ");
+        }
+        else {
+            System.out.println(" Установите версию приложения для IOS по ссылке. ");
+        }
+    }
+}
